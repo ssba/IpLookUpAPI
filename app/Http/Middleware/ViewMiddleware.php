32594e3efc485b40ea6bc4fec1a8231496976f7c
sbudumyan <s.budumyan@gmail.com>
2017-04-06 15:04:06 +0300

    IPAPI-3: Один из правильных вариантов

--- app/Http/Middleware/ViewMiddleware.php
+++ app/Http/Middleware/ViewMiddleware.php
@@ -16,11 +16,29 @@ class ViewMiddleware
      */
     public function handle($request, Closure $next)
     {
+        /*
         $response = $next($request);
         $statusCode = $response->getStatusCode(); 
         $text = $response->body;
         $codeMessage = Response::$statusTexts[$statusCode]; 
         return Response::json(array('code' => '$statusCode', 'message' => '$codeMessage', 'body' => '$text'));
+        */
+
+        $response = $next($request);
+        $statusCode = $response->getStatusCode();
+
+        $body = [
+            "code" =>$statusCode,
+            "message" => Response::$statusTexts[$statusCode],
+            "body" => $response->body
+        ];
+
+        $JsonOptions = 0;
+        if(Config::get('app.debug', false))
+            $JsonOptions = JSON_NUMERIC_CHECK | JSON_BIGINT_AS_STRING | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+
+        response()->json($body, $statusCode, [], $JsonOptions);
+
 
     }
 }
